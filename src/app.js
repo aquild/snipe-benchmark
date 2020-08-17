98bc11ed3d2a7c7c6b92efec8ad90e5038eba127
Aquild <aquild@aquild.dev>
2020-08-17 09:48:14 -0700

    Created express app

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import NodeCache from "node-cache";
+
+const app = express();
+const cache = new NodeCache({
+  useClones: false,
+  stdTTL: 10 * 60,
+  checkperiod: 5 * 60,
+});
+const port = 8000;
+
+// Routes
+app.get("/:id", async (req, res) => {
+  let client = cache.get(req.params.id);
+  if (client == undefined) return res.status(404).send();
+  res.send(client);
+});
+
+app.post("/:id/setup", async (req, res) => {
+  cache.set(req.params.id, { time: req.body.time, earliest: -1 });
+  res.send();
+});
+
+app.get("/:id/spam", async (req, res) => {
+  let client = cache.get(req.params.id);
+  if (client == undefined) return res.status(404).send();
+
+  if (client.time == -1 && Date.now() > client.time) {
+    client.time = Date.now();
+    res.send();
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
