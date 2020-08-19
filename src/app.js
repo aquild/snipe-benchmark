7469b080c64dfece0c713e24391d9a5cda754aef
Aquild <aquild@aquild.dev>
2020-08-19 12:20:36 -0700

    Added status endpoint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import fastify from "fastify";
 import NodeCache from "node-cache";
 
-const app = fastify();
+const app = fastify({ logger: { level: "info" } });
 const cache = new NodeCache({
   useClones: false,
   stdTTL: 10 * 60,
@@ -13,7 +13,24 @@ app.get("/", async (_req, res) => {
   res.redirect("https://github.com/aquild/snipe-benchmark");
 });
 
-// Routes
+// Status
+app.get("/status", async (_req, res) => {
+  const cpu = process.cpuUsage();
+
+  setTimeout(
+    () =>
+      res.send({
+        status: "OK",
+        cpu: process.cpuUsage(cpu).user / (1000 * 1000),
+        mem: process.memoryUsage().heapUsed / (1000 * 1000),
+      }),
+    1000
+  );
+});
+
+process.cpuUsage;
+
+// Benchmark
 app.get("/:id", async (req, res) => {
   let client = cache.get(req.params.id);
   if (client == undefined) return res.status(404).send();
@@ -25,7 +42,7 @@ app.post("/:id", async (req, res) => {
     time: req.body.time,
     result: { early: 0, late: 0, delay: -1 },
   });
-  console.log(`Server registered benchmark: ${req.params.id}`);
+  app.log.info(`Server registered benchmark: ${req.params.id}`);
   res.send();
 });
 
@@ -39,11 +56,20 @@ app.get("/:id/snipe", async (req, res) => {
     client.result.late++;
     if (client.result.delay == -1) {
       client.result.delay = Date.now() - client.time;
-      console.log(`Server completed benchmark: ${req.params.id}`);
+      app.log.info(`Server completed benchmark: ${req.params.id}`);
     }
   }
   res.status(204).send();
 });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+const start = async () => {
+  try {
+    await app.listen(port, "0.0.0.0");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
